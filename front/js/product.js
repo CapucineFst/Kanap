274abe82d41415e8ee03f4401fbd343affd1bb0f
CapucineFst <99402521+CapucineFst@users.noreply.github.com>
2022-10-14 14:33:43 +0200

    Add files via upload

--- front/js/product.js
+++ front/js/product.js
@@ -1,34 +1,107 @@
-function productURL() {
-  // get the product id from current page URL
+/**  Extract the id of the product from the URL */
+function getProductId() {
   const url = new URL(window.location.href);
   const urlParams = new URLSearchParams(url.search);
   const productId = urlParams.get("id");
   return productId;
 }
 
+/** Create an element image and put the picture of the product in it
+* Add product name, price and description
+* Create an element option and put the available colors in it
+*/
 function productFetch() {
-  const productId = productURL() ;
-  fetch (`http://localhost:3000/api/products/${productId}`)
-  .then((response) => response.json())
+  const productId = getProductId();
+  fetch(`http://localhost:3000/api/products/${productId}`)
+      .then((response) => response.json())
       .then((data) => {
-        // Create an element image and put the picture of the product in this element
-        const productPicture = document.createElement("img");
-        document.querySelector(".item__img").appendChild (productPicture);
-        productPicture.src = data.imageUrl;
-        productPicture.alt = data.altTxt;
+          const productPicture = document.createElement("img");
+          document.querySelector(".item__img").appendChild(productPicture);
+          productPicture.src = data.imageUrl;
+          productPicture.alt = data.altTxt;
 
-        // Add product name
-        const productName = document.getElementById("title");
-        productName.textContent = data.name;
+          document.getElementById("title").textContent = data.name;
+          document.getElementById("price").textContent = data.price;
+          document.getElementById("description").textContent = data.description;
 
-        // Add product price
-        const productPrice = document.getElementById("price");
-        productPrice.textContent = data.price;
+          for (i = 0; i < data.colors.length; i++) {
+              const productColor = document.createElement("option");
+              document.querySelector("#colors").appendChild(productColor);
+              productColor.textContent = data.colors[i];
+          }
 
-        // Add product description
-        const productDescription = document.getElementById("description");
-        productDescription.textContent = data.description;
-        
       })
 }
 productFetch();
+
+/** Give the "Ajouter au panier" button a role
+* Make sure the quantity is between 1 and 100
+* Assign the values of the product in the cart
+* In case the product is already in the cart (using the id and the color) we add the quantity but make sure the customers can't put more than a 100 copies in the cart (50 products x3 for example)
+* If the product is not in the cart, we simply add it to the existing cart
+* If the cart was empty, we create it and add the product
+*/
+function addToCart() {
+  const choosenQuantity = document.getElementById("quantity");
+  document.getElementById("addToCart").addEventListener("click", () => {
+      if (
+          choosenQuantity.value > 0 &&
+          choosenQuantity.value <= 100
+      ) {
+          let product = {
+              id: getProductId(),
+              name: document.getElementById("title").textContent,
+              price: document.getElementById("price").textContent,
+              colors: document.getElementById("colors").value,
+              quantity: document.getElementById("quantity").value,
+              imgProduct: document.getElementsByClassName("item__img").src,
+          };
+
+          let products = JSON.parse(localStorage.getItem("cart"));
+
+          if (products) {
+              const isProductInCart = products.find(
+                  (productAlreadyInCart) =>
+                  productAlreadyInCart.id === product.id &&
+                  productAlreadyInCart.colors === product.colors
+              );
+              if (isProductInCart) {
+                  let newQuantity =
+                      parseInt(isProductInCart.quantity) + parseInt(product.quantity);
+                  if (newQuantity <= 100) {
+                      isProductInCart.quantity = newQuantity;
+                      localStorage.setItem("cart", JSON.stringify(products));
+                      goToCartPage();
+                  } else {
+                      window.alert("Vous ne pouvez pas acheter plus de 100 exemplaires")
+                  }
+
+              } else {
+                  products.push(product);
+                  localStorage.setItem("cart", JSON.stringify(products));
+                  goToCartPage();
+              }
+          } else {
+              products.push(product);
+              localStorage.setItem("cart", JSON.stringify(products));
+              goToCartPage();
+          }
+      } else {
+          window.alert("Le nombre d'articles doit être compris entre 0 et 100.")
+      }
+  });
+}
+
+/** Display an alert window to inform the customer the product has been added to the cart
+* Customer can click "ok" if they want to go to their cart
+*/
+function goToCartPage() {
+  if (
+      window.confirm(
+          "Votre produit a été ajouté au panier. Pour le consulter, cliquez sur OK."
+      )
+  ) {
+      window.location.href = "cart.html";
+  }
+}
+addToCart();
--- front/js/scripts.js
+++ front/js/scripts.js
@@ -1,44 +1,40 @@
-function cardsFetch() {
+/** Fetch the API 
+ * Loop to make sure we take all the informations we need to create the product card (link, image, name, description)
+ */
+ function cardsFetch() {
 
-  //Fetch the API
-  fetch('http://localhost:3000/api/products')
-      .then((response) => response.json())
-      .then((data) => {
-          console.log(data);
+    fetch('http://localhost:3000/api/products')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
 
-          //Loop to make sure we go through all products
-          for (i = 0; i < data.length; i++) {
+            for (i = 0; i < data.length; i++) {
 
-              const productSection = document.getElementById("items");
-    
-              const productLink = document.createElement("a");
-              productSection.appendChild(productLink);
-              //productLink.setAttribute = ("href", `product.html?id=${data[i]._id}`);
-              // Forme de lien qui marche
-              productLink.href = `product.html?id=${data[i]._id}`;
-              
+                const productSection = document.getElementById("items");
 
-              const productCard = document.createElement("article");
-              productLink.appendChild(productCard);
+                const productLink = document.createElement("a");
+                productSection.appendChild(productLink);
+                productLink.setAttribute("href", `product.html?id=${data[i]._id}`);
 
-              const productPicture = document.createElement("img");
-              productCard.appendChild(productPicture);
-              productPicture.setAttribute("src", data[i].imageUrl);
-              productPicture.setAttribute("alt", data[i].altTxt);
 
-              const productName = document.createElement("h3");
-              productCard.appendChild(productName);
-              productName.textContent = data[i].name;
+                const productCard = document.createElement("article");
+                productLink.appendChild(productCard);
 
-              const productDescription = document.createElement("p");
-              productCard.appendChild(productDescription);
-              productDescription.textContent = data[i].description;
-          }
-      })
+                const productPicture = document.createElement("img");
+                productCard.appendChild(productPicture);
+                productPicture.setAttribute("src", data[i].imageUrl);
+                productPicture.setAttribute("alt", data[i].altTxt);
+
+                const productName = document.createElement("h3");
+                productCard.appendChild(productName);
+                productName.textContent = data[i].name;
+
+                const productDescription = document.createElement("p");
+                productCard.appendChild(productDescription);
+                productDescription.textContent = data[i].description;
+            }
+        })
 }
-cardsFetch();
 
-//Make sure the DOM content is loaded 
-window.addEventListener('DOMContentLoaded', (event) => {
-  console.log('DOM fully loaded');
-});
+/** Starting the function when the DOM is fully loaded */
+window.addEventListener('DOMContentLoaded', cardsFetch);
